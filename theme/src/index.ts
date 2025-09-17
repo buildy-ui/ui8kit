2a5d5cd63a34c7f522951b9662355f3ed276ca43
alexy-os <agorbunoff.ru@gmail.com>
2025-09-17 23:50:52 +0300

    add theme

---
+++ theme/src/index.ts
@@ -0,0 +1,6 @@
+export { skyOSTheme } from './themes/skyOS';
+export { modernUITheme } from './themes/modernUI';
+export { lesseUITheme } from './themes/lesseUI';
+export type { SkyOSTheme } from './themes/skyOS';
+export type { ModernUITheme } from './themes/modernUI';
+export type { LesseUITheme } from './themes/lesseUI';
