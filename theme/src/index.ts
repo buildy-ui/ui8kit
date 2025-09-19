21a2a45d9ef62afbe9f0fd274da9ddd40a55428e
alexy-os <agorbunoff.ru@gmail.com>
2025-09-19 12:37:13 +0300

    Add ThemeProvider and useTheme exports to index.ts

--- theme/src/index.ts
+++ theme/src/index.ts
@@ -4,3 +4,6 @@ export { lesseUITheme } from './themes/lesseUI';
 export type { SkyOSTheme } from './themes/skyOS';
 export type { ModernUITheme } from './themes/modernUI';
 export type { LesseUITheme } from './themes/lesseUI';
+
+// providers
+export { ThemeProvider, useTheme } from './providers/ThemeProvider';
